fc59101575f6b09e669cde40edd8df4abe0c4cc8
Md Iqbal Hossain <mdiqbalhossain20775@gmail.com>
2024-01-07 22:44:15 +0600

    java Oop Practice.

---
+++ src/practice/Read_Only.java
@@ -0,0 +1,16 @@
+package practice;
+
+
+//read only class Example
+//A Java class which has only getter methods.
+public class Read_Only {
+     class Student {
+        //private data member
+        private String college = "AKG";
+
+        //getter method for college
+        public String getCollege() {
+            return college;
+        }
+    }
+}
---
+++ src/practice/Write_Only.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Write_Only {
+    //A Java class which has only setter methods.
+     class Student{
+        //private data member
+        private String college;
+        //getter method for college
+        public void setCollege(String college){
+            this.college=college;
+        }
+    }
+}
