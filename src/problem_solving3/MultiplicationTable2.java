f07a52f12e6f5f603086a9515efb807f1a88982a
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-02-24 17:30:47 +0600

    "some fun practice"

---
+++ src/problem_solving3/MultiplicationTable2.java
@@ -0,0 +1,23 @@
+package problem_solving3;
+
+import java.util.Scanner;
+
+public class MultiplicationTable2 {
+    // Print Multiplication table Program in java(For Any Number)
+    public static void main(String[] args) {
+        int a, b, c, d;
+        System.out.println("Enter range of numbers to print their multiplication table");
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        for (c = a; c <= b; c++) {
+            System.out.println("Multiplication table of "+c);
+            for (d = 1; d <= 10; d++) {
+                System.out.println(c+"*"+d+" = "+(c*d));
+            }
+        }
+
+
+    }
+
+}
