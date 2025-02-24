567df7b617c1d89398432f236b13c9ebab8a3f05
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-02-24 17:05:35 +0600

    "some fun practice"

---
+++ src/problem_solving3/EnhancedForLoop2.java
@@ -0,0 +1,12 @@
+package problem_solving3;
+
+public class EnhancedForLoop2 {
+    //For String
+    public static void main(String[] args) {
+        String languages[] = { "C", "C++", "Java", "Python", "Ruby"};
+        for (String sample: languages) {
+            System.out.println(sample);
+        }
+
+    }
+}
---
+++ src/problem_solving3/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package problem_solving3;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    //Print Multiplication table Program in java
+    public static void main(String[] args) {
+        int n, c;
+        System.out.println("Enter an integer to print it's multiplication table");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        System.out.println("Multiplication table of "+n+" is :-");
+        for ( c = 1 ; c <= 10 ; c++ )
+            System.out.println(n+"*"+c+" = "+(n*c));
+    }
+}
