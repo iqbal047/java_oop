8a691eff67c755345fe9924dffbfd1cc2da64070
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-02-24 16:12:21 +0600

    "some fun practice"

---
+++ src/problem_solving3/ForLoop.java
@@ -0,0 +1,11 @@
+package problem_solving3;
+
+public class ForLoop {
+   // Simple For Loop Program in Java
+   public static void main(String[] args) {
+       int c;
+       for (c = 1; c<=10; c++){
+           System.out.println(c);
+       }
+   }
+}
