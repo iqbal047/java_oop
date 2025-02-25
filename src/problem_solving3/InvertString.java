cbc46cc5d5c3da76ff71a754e77b3fdf94ca42ac
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-02-25 10:29:18 +0600

    "some fun practice"

---
+++ src/problem_solving3/InvertString.java
@@ -0,0 +1,10 @@
+package problem_solving3;
+
+public class InvertString {
+    //Using Internal java Methog
+    public static void main(String[] args) {
+        StringBuffer a = new StringBuffer("Java programming is fun");
+        System.out.println(a.reverse());
+    }
+
+}
