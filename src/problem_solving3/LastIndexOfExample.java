59d323dcf48477715b1311e7f3a300fa1aba5431
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-03-02 11:06:27 +0600

    "some fun practice"

---
+++ src/problem_solving3/LastIndexOfExample.java
@@ -0,0 +1,14 @@
+package problem_solving3;
+
+public class LastIndexOfExample {
+    //How to compare 2 string  in java programme
+    public static void main(String[] args) {
+        String s1="hello";
+        String s2="hello";
+        String s3="meklo";
+        String s4="hemlo";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}
---
+++ src/problem_solving3/StringEndwith.java
@@ -0,0 +1,12 @@
+package problem_solving3;
+
+public class StringEndwith {
+    //How to string width with specific char  in java Program
+    public static void main(String[] args) {
+        String s1="java by TechnoLamror";
+        System.out.println(s1.endsWith("r"));  //true
+        System.out.println(s1.endsWith("Lamror"));  //true
+        System.out.println(s1.endsWith("lamror"));//false
+    }
+
+}
