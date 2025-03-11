24ef86de4bbfb88acb69fb65dbbb29ca0f9f9e9e
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-03-11 16:09:17 +0600

    "some fun practice"

---
+++ src/problem_solving3/ReplaceAllExample2.java
@@ -0,0 +1,12 @@
+package problem_solving3;
+
+public class ReplaceAllExample2 {
+    // How to replace string with another string in
+    //java Program
+    public static void main(String[] args) {
+        String s1="My name is Rajendra. My name is lamror. My name is Technolamror.";
+        String replaceString=s1.replaceAll("is","was");//replaces all occurrences of "is" to"was"
+        System.out.println(replaceString);
+
+    }
+}
