5b26b25da282c993438cb6620a2eb64157231e28
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-03-12 10:06:50 +0600

    "some fun practice"

---
+++ src/problem_solving3/Methods.java
@@ -0,0 +1,22 @@
+package problem_solving3;
+
+public class Methods {
+    // Constructor method
+    Methods() {
+        System.out.println("Constructor method is called when an object of it's class is created");
+    }
+    // Main method where program execution begins
+    public static void main(String[] args) {
+        staticMethod();
+        Methods object = new Methods();
+        object.nonStaticMethod();
+    }
+    // Static method
+    static void staticMethod() {
+        System.out.println("Static method can be called without creating object");
+    }
+    // Non static method
+    void nonStaticMethod() {
+        System.out.println("Non static method must be called by creating an object");
+    }
+}
