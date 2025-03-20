126b65ffc959cb1a700cd4ca99875c1466d6b9da
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-03-20 10:56:22 +0600

    "some fun practice"

---
+++ src/problem_solving3/GarbageCollection.java
@@ -0,0 +1,13 @@
+package problem_solving3;
+
+public class GarbageCollection {
+    //How perform garbage collection in java Program
+    public static void main(String [] args) throws Exception
+    {
+        Runtime rs = Runtime.getRuntime();
+        System.out.println("Free memory in JVM before Garbage Collection = "+rs.freeMemory());
+                rs.gc();
+        System.out.println("Free memory in JVM after Garbage Collection = "+rs.freeMemory());
+    }
+
+}
---
+++ src/problem_solving3/IPAddress.java
@@ -0,0 +1,14 @@
+package problem_solving3;
+
+import java.net.InetAddress;
+
+public class IPAddress {
+    //How to get own IP Address in java Program
+
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println(InetAddress.getLocalHost());
+    }
+
+
+}
