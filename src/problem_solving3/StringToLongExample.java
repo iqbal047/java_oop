74df7d5b0bfea6225c4d1d68cde7bb4496b64d46
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-09 22:41:43 +0600

    "some fun practice"

---
+++ src/problem_solving3/StringToLongExample.java
@@ -0,0 +1,10 @@
+package problem_solving3;
+
+public class StringToLongExample {
+    //How to convert string to long in java
+    public static void main(String[] args) {
+        String s="9990449935";
+        long l=Long.parseLong(s);
+        System.out.println(l);
+    }
+}
