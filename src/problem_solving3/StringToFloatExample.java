4978defc91fe4ca1fcc48a74c4019c35b77e8671
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-09 22:43:36 +0600

    "some fun practice"

---
+++ src/problem_solving3/StringToFloatExample.java
@@ -0,0 +1,10 @@
+package problem_solving3;
+
+public class StringToFloatExample {
+    //How to convert string to float in java
+    public static void main(String[] args) {
+        String s="23.6";
+        float f=Float.parseFloat("23.6");
+        System.out.println(f);
+    }
+}
