5c015ad3d4f4e33bc90d580e9ecf08ea5a94c16f
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-09 22:45:06 +0600

    "some fun practice"

---
+++ src/problem_solving3/StringToDoubleExample.java
@@ -0,0 +1,10 @@
+package problem_solving3;
+
+public class StringToDoubleExample {
+    //StringToDoubleExample
+    public static void main(String[] args) {
+        String s="23.6";
+        double d=Double.parseDouble("23.6");
+        System.out.println(d);
+    }
+}
