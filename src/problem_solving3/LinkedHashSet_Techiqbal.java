6474ae71959a3ec49a69435ac54dc890daa27c23
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-15 12:01:29 +0600

    "some fun practice"

---
+++ src/problem_solving3/LinkedHashSet_Techiqbal.java
@@ -0,0 +1,20 @@
+package problem_solving3;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSet_Techiqbal {
+    //How to create LinkedHashSet program in java
+    public static void main(String[] args) {
+        LinkedHashSet<String> al=new LinkedHashSet<String>();
+
+        al.add("Rajendra");
+        al.add("Raja");
+        al.add("Ravi");
+        al.add("TechIqbal");
+        Iterator<String> itr=al.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
