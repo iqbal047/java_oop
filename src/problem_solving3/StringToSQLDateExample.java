a269e31a7c35cf8ea19f44310629b38e45fbf4f3
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-16 22:15:38 +0600

    "some fun practice"

--- src/problem_solving3/StringToSQLDateExample.java
+++ src/problem_solving3/StringToSQLDateExample.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 
 public class StringToSQLDateExample {
     public static void main(String[] args) {
-        String str = "2015-03-31";
+        String str = "2025-03-31";
         Date date = Date.valueOf(str); // Converting string into SQL date
         System.out.println(date);
     }
