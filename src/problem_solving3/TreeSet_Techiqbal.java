78772d3ad7d205cbed80ae0d101f31c91f179a36
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-17 12:06:26 +0600

    "some fun practice"

--- src/problem_solving3/TreeSet_Techiqbal.java
+++ src/problem_solving3/TreeSet_Techiqbal.java
@@ -1,8 +1,20 @@
 package problem_solving3;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class TreeSet_Techiqbal {
     // 91. How to create TreeSet program in java
     public static void main(String[] args) {
-
+        //Creating and adding elements
+        TreeSet<String> al=new TreeSet<String>();
+        al.add("Rajendra");
+        al.add("Raja");
+        al.add("Ravi");
+        al.add("Technolamror");  //ments
+        Iterator<String> itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
     }
 }
