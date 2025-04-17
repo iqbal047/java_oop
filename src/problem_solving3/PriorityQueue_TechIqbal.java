34a61f31283b71a012c8cc3ebb0f2203f4a18760
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-18 00:05:27 +0600

    "some fun practice"

--- src/problem_solving3/PriorityQueue_TechIqbal.java
+++ src/problem_solving3/PriorityQueue_TechIqbal.java
@@ -1,8 +1,23 @@
 package problem_solving3;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class PriorityQueue_TechIqbal {
     //92. How to create PriorityQueue program in java
     public static void main(String[] args) {
-
+        PriorityQueue<String> queue=new PriorityQueue<String>();
+        queue.add("Rajendra");
+        queue.add("Mahendra");
+        queue.add("Raja");
+        queue.add("Technolamror");
+        queue.add("Rahul");
+        System.out.println("head:"+queue.element());
+        System.out.println("head:"+queue.peek());
+        System.out.println("iterating the queue elements:");
+        Iterator itr=queue.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
     }
 }
