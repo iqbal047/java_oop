48f533abe70c13363f09c6e581fe7ac5a1043eb5
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-18 00:08:12 +0600

    "some fun practice"

--- src/problem_solving3/PriorityQueue_TechIqbal.java
+++ src/problem_solving3/PriorityQueue_TechIqbal.java
@@ -7,17 +7,24 @@ public class PriorityQueue_TechIqbal {
     //92. How to create PriorityQueue program in java
     public static void main(String[] args) {
         PriorityQueue<String> queue=new PriorityQueue<String>();
-        queue.add("Rajendra");
-        queue.add("Mahendra");
-        queue.add("Raja");
-        queue.add("Technolamror");
-        queue.add("Rahul");
+        queue.add("Nazmul");
+        queue.add("Adiat");
+        queue.add("Riaj");
+        queue.add("Techiqbal");
+        queue.add("Nawsish");
         System.out.println("head:"+queue.element());
         System.out.println("head:"+queue.peek());
         System.out.println("iterating the queue elements:");
         Iterator itr=queue.iterator();
         while(itr.hasNext()){
             System.out.println(itr.next());
+        }
+        queue.remove();
+        queue.poll();
+        System.out.println("after removing two elements:");
+        Iterator<String> itr2=queue.iterator();
+        while(itr2.hasNext()){
+            System.out.println(itr2.next());
         }
     }
 }
