989636e928b5156cdb781cc827227f24408ea4d8
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-20 11:57:17 +0600

    "some fun practice"

--- src/problem_solving3/MapInterfaceExample.java
+++ src/problem_solving3/MapInterfaceExample.java
@@ -1,8 +1,16 @@
 package problem_solving3;
 
+import java.util.Map;
+
 public class MapInterfaceExample {
     // 93. How to create HashMap using map interface program in java
     public static void main(String[] args) {
-
+        Map<Integer,String> map=new HashMap<Integer,String>();
+        map.put(100,"Rajendra");
+        map.put(101,"Lamror");
+        map.put(102,"Technolamror");
+        for(Map.Entry m:map.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
     }
 }
