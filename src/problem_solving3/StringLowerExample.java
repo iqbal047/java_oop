c2481afb95b9b9cb5d56129ed0f06ea5e389c270
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-04-26 11:18:14 +0600

    "some fun practice"

--- src/problem_solving3/StringLowerExample.java
+++ src/problem_solving3/StringLowerExample.java
@@ -1,8 +1,9 @@
 package problem_solving3;
 
 public class StringLowerExample {
+    // 41. How to convert all char in string lower case in java Program
     public static void main(String[] args) {
-        String s1="TECHNOLAMROR by Rajendralamror HELLO stRIng";
+        String s1="TECHNOIQBAL by RehanIqbal HELLO stRIng";
         String s1lower=s1.toLowerCase();
         System.out.println(s1lower);
     }
