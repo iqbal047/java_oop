1f606ca58a10e2e89c5831979dcaa0cc5b7b1617
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-05-01 11:47:48 +0600

    "some fun practice"

--- src/problem_solving3/Allocate.java
+++ src/problem_solving3/Allocate.java
@@ -1,6 +1,7 @@
 package problem_solving3;
 
 public class Allocate {
+    //51. Advantage of Finally in Exception Handling java Program
     public static void main(String[] args) {
         try {
             long data[] = new long[1000000000];
