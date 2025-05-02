c1c6b7b1940e20e5bca903026455ca360b8b8177
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-05-02 23:44:41 +0600

    "some fun practice"

--- src/problem_solving3/RandomNumbers.java
+++ src/problem_solving3/RandomNumbers.java
@@ -3,6 +3,7 @@ package problem_solving3;
 import java.util.Random;
 
 public class RandomNumbers {
+    //56. How to Generate random number in java Program
     public static void main(String[] args) {
         int c;
         Random t = new Random();
