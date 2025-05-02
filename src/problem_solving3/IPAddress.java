5e38933d78c12a9368e6dbdbd7305d8842db83ca
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-05-02 23:51:17 +0600

    "some fun practice"

--- src/problem_solving3/IPAddress.java
+++ src/problem_solving3/IPAddress.java
@@ -5,7 +5,7 @@ import java.net.InetAddress;
 public class IPAddress {
     //58. How to get own IP Address in java Program
 
-    public static void main(String args[]) throws Exception
+    public static void main(String[] args) throws Exception
     {
         System.out.println(InetAddress.getLocalHost());
     }
